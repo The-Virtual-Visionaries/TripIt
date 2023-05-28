c9de34ce2bdccd5c92d90b45e62b5f4134e1adb1
kavantan <kavantan10@gmail.com>
2023-05-28 14:44:38 +0800

    Modify activity recommender based on new discussed specifications

--- src/activitypage/ActivityRecommender.js
+++ src/activitypage/ActivityRecommender.js
@@ -12,22 +12,21 @@ const ActivityRecommender = () => {
   const handleOnDragEnd = (result) => {
     if (!result.destination) return;
 
-    const sourceDay = data.find(
-      (day) => day.day.toString() === result.source.droppableId
-    );
-    const destDay = data.find(
-      (day) => day.day.toString() === result.destination.droppableId
-    );
+    const [sourceDay, sourceTime] = result.source.droppableId.split("-");
+    const [destDay, destTime] = result.destination.droppableId.split("-");
 
-    const [removed] = sourceDay.activities.splice(result.source.index, 1);
-    destDay.activities.splice(result.destination.index, 0, removed);
+    const sourceDayData = data.find((day) => day.day.toString() === sourceDay);
+    const destDayData = data.find((day) => day.day.toString() === destDay);
+
+    const [removed] = sourceDayData[sourceTime].splice(result.source.index, 1);
+    destDayData[destTime].splice(result.destination.index, 0, removed);
 
     setData([...data]);
   };
 
-  const deleteActivity = (dayId, activityIndex) => {
+  const deleteActivity = (dayId, time, activityIndex) => {
     const day = data.find((day) => day.day === dayId);
-    day.activities.splice(activityIndex, 1);
+    day[time].splice(activityIndex, 1);
     setData([...data]);
   };
 
@@ -39,49 +38,56 @@ const ActivityRecommender = () => {
     <>
       <Navbar />
       <DragDropContext onDragEnd={handleOnDragEnd}>
-        {data.map((day, index) => (
-          <Droppable droppableId={day.day.toString()} key={index}>
-            {(provided) => (
-              <div
-                className={styles.activityContainer}
-                {...provided.droppableProps}
-                ref={provided.innerRef}
-              >
-                <h2 className={styles.title}>Day {day.day}</h2>
-                {day.activities.map((activity, activityIndex) => (
-                  <Draggable
-                    key={activity.activity_type}
-                    draggableId={activity.activity_type}
-                    index={activityIndex}
-                  >
-                    {(provided) => (
-                      <div
-                        className={styles.root}
-                        {...provided.draggableProps}
-                        {...provided.dragHandleProps}
-                        ref={provided.innerRef}
-                      >
-                        <div>
-                          <h3>{activity.activity_type}</h3>
-                          <p>{activity.description}</p>
-                          <button
-                            className={styles.deleteButton}
-                            onClick={() =>
-                              deleteActivity(day.day, activityIndex)
-                            }
-                          >
-                            Delete
-                          </button>
+        {data.map((day, index) =>
+          ["Morning", "Afternoon", "Night"].map((time) => (
+            <Droppable
+              droppableId={`${day.day}-${time}`}
+              key={`${index}-${time}`}
+            >
+              {(provided) => (
+                <div
+                  className={styles.activityContainer}
+                  {...provided.droppableProps}
+                  ref={provided.innerRef}
+                >
+                  <h2 className={styles.title}>
+                    Day {day.day} - {time}
+                  </h2>
+                  {day[time].map((activity, activityIndex) => (
+                    <Draggable
+                      key={activity.activity_type}
+                      draggableId={activity.activity_type}
+                      index={activityIndex}
+                    >
+                      {(provided) => (
+                        <div
+                          className={styles.root}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          ref={provided.innerRef}
+                        >
+                          <div>
+                            <h3>{activity.activity_type}</h3>
+                            <p>{activity.description}</p>
+                            <button
+                              className={styles.deleteButton}
+                              onClick={() =>
+                                deleteActivity(day.day, time, activityIndex)
+                              }
+                            >
+                              Delete
+                            </button>
+                          </div>
                         </div>
-                      </div>
-                    )}
-                  </Draggable>
-                ))}
-                {provided.placeholder}
-              </div>
-            )}
-          </Droppable>
-        ))}
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          ))
+        )}
         <button className={styles.saveButton} onClick={handleSaveItinerary}>
           Save Itinerary
         </button>
