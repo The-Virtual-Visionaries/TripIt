cb6bec3df33a39594b7c04c04ce8de8770d113f8
KevinEyo1 <eyokaiwenkevin@gmail.com>
2023-05-28 15:33:28 +0800

    Add changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import CreateItinerary from "./createpage/CreateItinerary";
+import Itineraries from "./itinerariespage/Itineraries";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/create" element={<CreateItinerary />} />
+        <Route path="/itineraries" element={<Itineraries />} />
       </Routes>
     </BrowserRouter>
   );
